e0b8c2ca33b7d8758c7a0463ce43da59b4acede5
Vlad Slivilescu <vlad.slivilescu@gmail.com>
2022-01-08 16:01:51 +0200

    [BACKEND] Remove unnecessary newline

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
