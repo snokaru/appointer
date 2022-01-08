4274af0e0fd89401025414e16f9775d698c317d8
Vlad Slivilescu <vlad.slivilescu@gmail.com>
2022-01-08 16:15:10 +0200

    [FRONTEND] Remove unnecessary logging

--- frontend/src/features/auth/services.js
+++ frontend/src/features/auth/services.js
@@ -1,11 +1,7 @@
 import axios from 'axios'
         
 export async function login(email, password) {
-    console.log('making login request')
     const tokens = await getTokens(email, password)
-    console.log('request fulfiflled with')
-    console.log(tokens)
-
     updateTokens(tokens.access, tokens.refresh)
 
     return await getCurrentUser()
@@ -25,7 +21,6 @@ export function updateTokens(access, refresh) {
 
 export async function getTokens(email, password) {
     const response = await axios.post('/api/auth/', { email, password })
-    console.log(response)
     return response.data
 }
 
