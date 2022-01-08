f37d525b1914beaa29684b9473fd8f24ea2c3c20
Vlad Slivilescu <vlad.slivilescu@gmail.com>
2022-01-08 16:11:25 +0200

    [BACKEND] Provide user model with different user types

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,7 +4,15 @@ from django.db import models
 # Create your models here.
 
 class User(AbstractUser):
+    USER_TYPES = (
+        (1, 'user'),
+        (2, 'bussiness'),
+        (3, 'admin'),
+    )
+
     email = models.EmailField("email", unique=True)
+    type = models.PositiveSmallIntegerField(choices=USER_TYPES) 
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "password",]
+
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,15 +2,34 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
-class UserSerializer(serializers.ModelSerializer):
+# custom choice field to work with display names
+class ChoiceField(serializers.ChoiceField):
 
-    password = serializers.CharField(write_only=True)
+    def to_representation(self, data):
+        if data not in self.choices.keys():
+            self.fail('invalid_choice', input=data)
+        else:
+            return self.choices[data]
+
+    def to_internal_value(self, data):
+        for key, value in self.choices.items():
+            if value == data:
+                 return key
+        self.fail('invalid_choice', input=data)
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
+    type = ChoiceField(choices=get_user_model().USER_TYPES)
+
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'email', 'password', 'type']
 
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data['password'])
         return super(UserSerializer, self).create(validated_data)
-
-    class Meta:
-        model = get_user_model()
-        fields = ['id', 'email', 'password',]
 
